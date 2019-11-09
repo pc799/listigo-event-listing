0f3e021ee7662aebf8bf987e1ae1524eafb3ba07
pc799 <imfclaws@gmail.com>
2019-11-09 18:00:31 +0530

    Database init

--- res/config.php
+++ res/config.php
@@ -1,8 +1,8 @@
 <?php
-    define("DB_HOST", "localhost");
-    define("DB_USER", "root");
-    define("DB_PASS", "");
-    define("DB_NAME", "listigo");
+    define("DB_HOST", "m60mxazb4g6sb4nn.chr7pe7iynqr.eu-west-1.rds.amazonaws.com");
+    define("DB_USER", "f4yj08g2imj142qq");
+    define("DB_PASS", "tcvf4i2hobj2wxuu");
+    define("DB_NAME", "tcjcis2qqlpluhms");
     
     $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME) or die("Connection to database failed!");
 ?>
